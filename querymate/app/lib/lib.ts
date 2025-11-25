f89b3fc452d6200cf14c49c3b48e394cb5aca0f0
Jaswanth1406 <jaswanthprasanna68@gmail.com>
2025-11-25 13:26:07 +0530

    Completed better auth and conversation creation

---
+++ querymate/app/lib/lib.ts
@@ -0,0 +1,11 @@
+// lib/db.ts
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./schema";
+
+export const pool = new Pool({
+  connectionString: process.env.SUPABASE_DB_URL,
+});
+
+export const db = drizzle(pool, { schema });
+
